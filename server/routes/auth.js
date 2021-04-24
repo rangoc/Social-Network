6cafc97031702da7624478dd18e1537b26df10ad
goranc <cabarkapa.goran@gmail.com>
2021-04-24 13:59:53 +0200

    Adding a user works, just testing

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
+const User = require('../models/User');
+// REGISTER
+router.get('/register', async (req, res) => {
+  const user = new User({
+    username: 'john',
+    email: 'john@gmail.com',
+    password: '123456',
+  });
 
-router.get('/', (req, res) => {
-  res.send('hey its auth route');
+  await user.save();
+  res.send('ok');
 });
 module.exports = router;

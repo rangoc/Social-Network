a4ae05f8fdc1e0a92b6dfe3a979c941157654c2c
goranc <cabarkapa.goran@gmail.com>
2021-04-25 22:13:08 +0200

    Added functionality for logging in user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -22,4 +22,24 @@ router.post('/register', async (req, res) => {
     console.log(err);
   }
 });
+
+// LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    !user && res.status(404).send('user not found');
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    !validPassword && res.status(400).json('wrong password');
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
 module.exports = router;

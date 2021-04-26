c71d66a25f997be22b57a14543408ca5ac0f10e9
goranc <cabarkapa.goran@gmail.com>
2021-04-26 12:29:59 +0200

    Deleting user successfully

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 
 // update user
 router.put('/:id', async (req, res) => {
-  if (req.body.userId === req.params.id || req.user.isAdmin) {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
     // if password is being updated
     if (req.body.password) {
       try {
@@ -30,7 +30,20 @@ router.put('/:id', async (req, res) => {
     return res.status(403).json('You can update only your account');
   }
 });
+
 // delete user
+router.delete('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json('Account has been deleted sucessfully');
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json('You can delete only your account');
+  }
+});
 // get a user
 // follow user
 // unfollow user

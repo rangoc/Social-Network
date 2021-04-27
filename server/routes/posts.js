e18d8a0314a94454d829baa160ff3c7739bf82eb
goranc <cabarkapa.goran@gmail.com>
2021-04-27 15:54:12 +0200

    Added functionality for liking/disliking posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -38,7 +38,21 @@ router.delete('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-// like a post
+// like/dislike a post
+router.put('/:id/like', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json('The post has been successfully liked');
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json('The post has been disliked');
+    }
+  } catch (err) {
+    res.status(500);
+  }
+});
 // get a post
 // get timeline posts of all "followers" posts
 module.exports = router;

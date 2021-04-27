3e67ff376e3e1745702cf0012d1d1c5154882930
goranc <cabarkapa.goran@gmail.com>
2021-04-27 15:57:20 +0200

    Added functionality for getting single post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -54,5 +54,13 @@ router.put('/:id/like', async (req, res) => {
   }
 });
 // get a post
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // get timeline posts of all "followers" posts
 module.exports = router;

551981387721a628b56350ff55340ecd8be1304b
goranc <cabarkapa.goran@gmail.com>
2021-04-28 15:33:49 +0200

    Style for topbar done

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // components
-import App from 'components/App';
+import App from 'App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Topbar from 'components/topbar/Topbar';
+const Home = () => {
+  return <Topbar />;
+};
+
+export default Home;

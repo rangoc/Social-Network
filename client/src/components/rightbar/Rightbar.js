7146b1849dc6b92ed3399dd5fdd9be7c08681340
goranc <cabarkapa.goran@gmail.com>
2021-05-05 11:08:55 +0200

    Added styling for rightbar, online friends section

--- client/src/components/rightbar/Rightbar.js
+++ client/src/components/rightbar/Rightbar.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import './rightbar.scss';
 const Rightbar = () => {
-  return <div className="rightbar">rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="/assets/gift.png" alt="birthday" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img className="rightbarAd" src="/assets/ad.png" alt="ad" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt="profile"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">John Carter</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;

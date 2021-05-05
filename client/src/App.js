56ce5ea7047385d851af9addce2c63ffb85d6281
goranc <cabarkapa.goran@gmail.com>
2021-05-05 14:33:24 +0200

    Removed css, added scss for App

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from 'pages/home/Home';
+import './app.scss';
 // import Profile from 'pages/profile/Profile';
 // import Login from 'pages/login/Login';
 // import Register from 'pages/register/Register';

075aba58618e65ec6dfd7594ae70041d254f6af8
goranc <cabarkapa.goran@gmail.com>
2021-05-14 12:58:57 +0200

    Implemented private messages being sent trough socket.io

--- socket/index.js
+++ socket/index.js
@@ -14,17 +14,32 @@ const addUser = (userId, socketId) => {
 const removeUser = (socketId) => {
   users = users.filter((user) => user.socketId !== socketId);
 };
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
 io.on('connection', (socket) => {
+  // when connected
   console.log('a user connected.');
   // emmit a message to all client users, from server
   // io.emit('welcome', 'hello this is socket server');
 
-  // take userId and socketId from user
+  //take userId and socketId from user
   socket.on('addUser', (userId) => {
     addUser(userId, socket.id);
     io.emit('getUsers', users);
   });
 
+  //send and get message
+  socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit('getMessage', {
+      senderId,
+      text,
+    });
+  });
+
+  //when disconnect
   socket.on('disconnect', () => {
     console.log('a user disconnected!');
     removeUser(socket.id);
